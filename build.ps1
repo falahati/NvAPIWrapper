b127072fb6254292dd61cd3fc2f511eb16c80fc8
s_falahati <s_falahati@hotmail.com>
2017-04-16 21:51:02 +0430

    NuGet package generation script added

---
+++ build.ps1
@@ -0,0 +1,2 @@
+$msbuild = join-path -path (Get-ItemProperty "HKLM:\software\Microsoft\MSBuild\ToolsVersions\14.0")."MSBuildToolsPath" -childpath "msbuild.exe"
+&$msbuild NvAPIWrapper\NvAPIWrapper.csproj /t:Build /t:Package /p:Configuration="Release"
