401469cc200976cd6c0b73c0e31d0442b392e40c
s_falahati <s_falahati@hotmail.com>
2020-04-17 00:47:43 +0430

    Fixed multiple problems with internal `GetBits()` and `SetBits()` extension method

--- NvAPIWrapper/Native/Helpers/ExtensionMethods.cs
+++ NvAPIWrapper/Native/Helpers/ExtensionMethods.cs
@@ -113,23 +113,27 @@ namespace NvAPIWrapper.Native.Helpers
         public static bool GetBit<T>(this T integer, int index) where T : struct, IConvertible
         {
             var bigInteger = BitWiseConvert<ulong, T>(integer);
+            var mask = 1ul << index;
 
-            return (bigInteger & (1ul << index)) != 0;
+            return (bigInteger & mask) > 0;
         }
 
         public static ulong GetBits<T>(this T integer, int index, int count) where T : struct, IConvertible
         {
-            var mask = 0ul;
-
-            for (var i = 0; i < count; i++)
-            {
-                mask = mask | (1ul << (index + i));
-            }
-
             var bigInteger = BitWiseConvert<ulong, T>(integer);
 
-            return bigInteger & mask;
+            if (index > 0)
+            {
+                bigInteger >>= index;
+            }
+
+            count = 64 - count;
+            bigInteger <<= count;
+            bigInteger >>= count;
+
+            return bigInteger;
         }
+
 
         // ReSharper disable once FunctionComplexityOverflow
         // ReSharper disable once ExcessiveIndentation
@@ -226,8 +230,9 @@ namespace NvAPIWrapper.Native.Helpers
 
         public static T SetBit<T>(this T integer, int index, bool value) where T : struct, IConvertible
         {
-            var mask = 1ul << index;
             var bigInteger = BitWiseConvert<ulong, T>(integer);
+
+            var mask = 1ul << index;
             var newInteger = value ? bigInteger | mask : bigInteger & ~mask;
 
             return BitWiseConvert<T, ulong>(newInteger);
@@ -236,17 +241,15 @@ namespace NvAPIWrapper.Native.Helpers
         // ReSharper disable once TooManyArguments
         public static T SetBits<T>(this T integer, int index, int count, ulong value) where T : struct, IConvertible
         {
-            var mask = 0ul;
-
-            for (var i = 0; i < count; i++)
-            {
-                mask = mask | (1ul << (index + i));
-            }
-
             var bigInteger = BitWiseConvert<ulong, T>(integer);
-            var newInteger = bigInteger | (mask & value);
 
-            return BitWiseConvert<T, ulong>(newInteger);
+            count = 64 - count;
+            value <<= count;
+            value >>= count - index;
+
+            bigInteger |= value;
+
+            return BitWiseConvert<T, ulong>(bigInteger);
         }
 
         public static byte[] ToByteArray<T>(this T structure) where T : struct
@@ -277,7 +280,7 @@ namespace NvAPIWrapper.Native.Helpers
             {
                 Marshal.Copy(byteArray, 0, pointer, Math.Min(byteArray.Length, size));
 
-                return (T)Marshal.PtrToStructure(pointer, typeof(T));
+                return (T) Marshal.PtrToStructure(pointer, typeof(T));
             }
             finally
             {
